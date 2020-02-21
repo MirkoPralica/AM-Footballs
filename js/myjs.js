406310ead32215857d49f630eb7b2861093ef251
Ahmad Yassin <akyassin@google.com>
2020-02-21 20:07:24 +0100

    Website developing

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,16 @@
+    function getlogo(link) {
+        var img = link;
+        var appear_image = `<div id="appear_image_div" onclick="closeImage()"><img src="${img}" alt=""></div>`;
+        console.log(appear_image);
+        document.getElementById("spain-division").innerHTML = appear_image;
+    }
+
+
     function insertTable(table, cls, myTable) {
+        var logoimg = table.team.crestUrl;
         myTable += `<tr>`;
         myTable += `<td ${cls}> ${ table.position } </td>`;
-        myTable += `<td ${cls}> ${ table.team.name } </td>`;
+        myTable += `<td ${cls}> <a id="logoimg" href="#" onclick="getlogo('${logoimg}')"> ${ table.team.name } </a> </td>`;
         myTable += `<td ${cls}> ${ table.playedGames } </td>`;
         myTable += `<td ${cls}> ${ table.won } </td>`;
         myTable += `<td ${cls}> ${ table.draw } </td>`;
@@ -15,7 +24,7 @@
         return myTable;
     }
 
-    function tableAPIs() {
+    function tableAPIsSL() {
         const url = "http://api.football-data.org/v2/competitions/2014/standings";
         fetch(url, {
                 method: "GET",
@@ -34,25 +43,25 @@
 
                 let output = "";
                 output += `<p>Season ${season}, Started on ${data.season.startDate}, 
-                            ending on ${data.season.endDate}</p>`;
+                                ending on ${data.season.endDate}</p>`;
 
                 document.getElementById("title-spain").innerHTML = output;
 
                 let myTable = "";
                 myTable = `<table class="table table-striped table-sm" " style="margin-top:20px;">
-                           <thead class="thead-dark ">
-                           <tr>
-                           <th scope="col ">#</th>
-                           <th scope="col ">Team</th>
-                           <th scope="col ">Played</th><th scope="col ">Win</th>
-                           <th scope="col ">Draw</th>
-                           <th scope="col ">Loss</th>
-                           <th scope="col ">Remaining</th>
-                           <th scope="col ">Goals F</th>
-                           <th scope="col ">Goals A</th>
-                           <th scope="col ">Goals Diff</th>
-                           <th scope="col ">Points</th>
-                           </tr></thead><tbody>`
+                            <thead class="thead-dark ">
+                            <tr>
+                            <th scope="col ">#</th>
+                            <th scope="col ">Team</th>
+                            <th scope="col ">Played</th><th scope="col ">Win</th>
+                            <th scope="col ">Draw</th>
+                            <th scope="col ">Loss</th>
+                            <th scope="col ">Remaining</th>
+                            <th scope="col ">Goals F</th>
+                            <th scope="col ">Goals A</th>
+                            <th scope="col ">Goals Diff</th>
+                            <th scope="col ">Points</th>
+                            </tr></thead><tbody>`
 
 
                 for (let i = 0; i < data.standings[0].table.length; i++) {
@@ -68,11 +77,6 @@
                 myTable += '</tr></tbody></table>';
 
                 document.getElementById("spain-division").innerHTML = myTable;
-
-                let boxdivs = "";
-                boxdivs = `<div class="" " style="margin-top:20px;">
-                           <div style="width:25px"></div>`
-
             })
             .catch(function(error) {
                 console.log(error);
@@ -81,12 +85,11 @@
 
     function clearDiv() {
         document.getElementById("spain-division").innerHTML = "";
-        document.getElementById("title-spain").innerHTML = "";   
+        document.getElementById("title-spain").innerHTML = "";
         console.log("Spain");
-             
     }
 
-    function ScoreAPIs() {
+    function ScoreAPIsSL() {
         const url = "https://api.football-data.org/v2/competitions/PD/scorers";
         fetch(url, {
                 method: "GET",
@@ -105,19 +108,19 @@
 
                 let output = "";
                 output += `<p>Season ${season}, Started on ${data.season.startDate}, 
-                            ending on ${data.season.endDate}</p>`;
+                                ending on ${data.season.endDate}</p>`;
 
                 document.getElementById("title-spain").innerHTML = output;
 
                 let scoreTable = "";
                 scoreTable = `<table class="table table-striped table-sm" " style="margin-top:20px;">
-                           <thead class="thead-dark ">
-                           <tr>
-                           <th scope="col ">#</th>
-                           <th scope="col ">Name</th>
-                           <th scope="col ">Goals</th>
-                           <th scope="col ">Team</th>
-                           </tr></thead><tbody>`
+                            <thead class="thead-dark ">
+                            <tr>
+                            <th scope="col ">#</th>
+                            <th scope="col ">Name</th>
+                            <th scope="col ">Goals</th>
+                            <th scope="col ">Team</th>
+                            </tr></thead><tbody>`
                 let x = 1;
                 for (let i = 0; i < data.scorers.length; i++) {
                     scoreTable += `<tr>`;
@@ -133,10 +136,6 @@
                 scoreTable += '</tr></tbody></table>';
 
                 document.getElementById("spain-division").innerHTML = scoreTable;
-
-                boxdivs = `<div class="" " style="margin-top:20px;">
-                           <div style="width:25px"></div>`
-
             })
             .catch(function(error) {
                 console.log(error);
@@ -162,25 +161,25 @@
 
                 let output = "";
                 output += `<p>Season ${season}, Started on ${data.season.startDate}, 
-                            ending on ${data.season.endDate}</p>`;
+                                ending on ${data.season.endDate}</p>`;
 
                 document.getElementById("title-england").innerHTML = output;
 
                 let myTable = "";
                 myTable = `<table class="table table-striped table-sm" " style="margin-top:20px;">
-                           <thead class="thead-dark ">
-                           <tr>
-                           <th scope="col ">#</th>
-                           <th scope="col ">Team</th>
-                           <th scope="col ">Played</th><th scope="col ">Win</th>
-                           <th scope="col ">Draw</th>
-                           <th scope="col ">Loss</th>
-                           <th scope="col ">Remaining</th>
-                           <th scope="col ">Goals F</th>
-                           <th scope="col ">Goals A</th>
-                           <th scope="col ">Goals Diff</th>
-                           <th scope="col ">Points</th>
-                           </tr></thead><tbody>`
+                            <thead class="thead-dark ">
+                            <tr>
+                            <th scope="col ">#</th>
+                            <th scope="col ">Team</th>
+                            <th scope="col ">Played</th><th scope="col ">Win</th>
+                            <th scope="col ">Draw</th>
+                            <th scope="col ">Loss</th>
+                            <th scope="col ">Remaining</th>
+                            <th scope="col ">Goals F</th>
+                            <th scope="col ">Goals A</th>
+                            <th scope="col ">Goals Diff</th>
+                            <th scope="col ">Points</th>
+                            </tr></thead><tbody>`
 
 
                 for (let i = 0; i < data.standings[0].table.length; i++) {
@@ -196,11 +195,6 @@
                 myTable += '</tr></tbody></table>';
 
                 document.getElementById("england-division").innerHTML = myTable;
-
-                let boxdivs = "";
-                boxdivs = `<div class="" " style="margin-top:20px;">
-                           <div style="width:25px"></div>`
-
             })
             .catch(function(error) {
                 console.log(error);
@@ -233,19 +227,19 @@
 
                 let output = "";
                 output += `<p>Season ${season}, Started on ${data.season.startDate}, 
-                            ending on ${data.season.endDate}</p>`;
+                                ending on ${data.season.endDate}</p>`;
 
                 document.getElementById("title-england").innerHTML = output;
 
                 let scoreTable = "";
                 scoreTable = `<table class="table table-striped table-sm" " style="margin-top:20px;">
-                           <thead class="thead-dark ">
-                           <tr>
-                           <th scope="col ">#</th>
-                           <th scope="col ">Name</th>
-                           <th scope="col ">Goals</th>
-                           <th scope="col ">Team</th>
-                           </tr></thead><tbody>`
+                            <thead class="thead-dark ">
+                            <tr>
+                            <th scope="col ">#</th>
+                            <th scope="col ">Name</th>
+                            <th scope="col ">Goals</th>
+                            <th scope="col ">Team</th>
+                            </tr></thead><tbody>`
                 let x = 1;
                 for (let i = 0; i < data.scorers.length; i++) {
                     scoreTable += `<tr>`;
@@ -261,32 +255,27 @@
                 scoreTable += '</tr></tbody></table>';
 
                 document.getElementById("england-division").innerHTML = scoreTable;
-
-                let boxdivs = "";
-                boxdivs = `<div class="" " style="margin-top:20px;">
-                           <div style="width:25px"></div>`
-
             })
             .catch(function(error) {
                 console.log(error);
             });
     }
-    
 
-    document.addEventListener('DOMContentLoaded', function () {
+
+    document.addEventListener('DOMContentLoaded', function() {
         var checkbox = document.querySelector('input[type="checkbox"]');
-      
-        checkbox.addEventListener('change', function () {
-          if (checkbox.checked) {
-              document.getElementById("Spain").classList.remove("section3-light");
-              document.getElementById("Spain").classList.add("section3-dark");
-              document.getElementById("England").classList.remove("section3-light");
-              document.getElementById("England").classList.add("section3-dark");
+
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+                document.getElementById("Spain").classList.remove("section-light");
+                document.getElementById("Spain").classList.add("section-dark");
+                document.getElementById("England").classList.remove("section-light");
+                document.getElementById("England").classList.add("section-dark");
             } else {
-                document.getElementById("Spain").classList.remove("section3-dark");
-                document.getElementById("Spain").classList.add("section3-light");
-                document.getElementById("England").classList.remove("section3-dark");
-                document.getElementById("England").classList.add("section3-light");
-          }
+                document.getElementById("Spain").classList.remove("section-dark");
+                document.getElementById("Spain").classList.add("section-light");
+                document.getElementById("England").classList.remove("section-dark");
+                document.getElementById("England").classList.add("section-light");
+            }
         });
-      });
+    });

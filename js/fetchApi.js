315930a1c8745bbdf7b4271c5b96d8b62c4bd917
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-02-25 10:30:19 +0100

    Files

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -1,5 +1,6 @@
- window.onload = tableAPIs(2014, 'spain');
- window.onload = tableAPIs(2021, 'england');
+window.onload = tableAPIs(2014, 'spain');
+window.onload = tableAPIs(2021, 'england');
+
 
 
 function getlogo(link) {
@@ -32,6 +33,8 @@ function insertTable(table, cls, myTable) {
     myTable += `<tr>`;
     return myTable;
 }
+
+
 
 function tableAPIs(code, country) {
     let primaryApi = `http://api.football-data.org/v2/competitions/${ code }/standings`;
@@ -149,110 +152,4 @@ function ScoreAPIs(code, country) {
         .catch(function(error) {
             console.log(error);
         });
-}
-
-$('#press').change(function(){
-    if(this.checked) {
-        $('.section-dark').removeClass("section-light");
-        $('.section-light').addClass("section-dark");
-    }
-    else {
-        $('.section-light').removeClass('section-dark');
-        $('section-dark').addClass('section-light');
-    }
-});
-
-
-function FormValidation() {
-    //First Name Validation 
-    var fn = document.getElementById('firstname').value;
-    if (fn == "") {
-        alert('Please Enter First Name');
-        document.getElementById('firstname').style.borderColor = "red";
-        return false;
-    } else {
-        document.getElementById('firstname').style.borderColor = "green";
-    }
-    if (/^[0-9]+$/.test(document.getElementById("firstname").value)) {
-        alert("First Name Contains Numbers!");
-        document.getElementById('firstname').style.borderColor = "red";
-        return false;
-    } else {
-        document.getElementById('firstname').style.borderColor = "green";
-    }
-    if (fn.length <= 2) {
-        alert('Your Name is To Short');
-        document.getElementById('firstname').style.borderColor = "red";
-        return false;
-    } else {
-        document.getElementById('firstname').style.borderColor = "green";
-    }
-};
-
-function submitmethod() {
-    // show Loading in submit-success
-    document.getElementById("submit-success").innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
-    setTimeout(function() {
-        // this will remove the loading and show the success message
-        document.getElementById("submit-message").innerHTML =
-            `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                Your submit has succeed..!
-            </div>`
-        document.getElementById("submit-success").innerHTML = "";
-        document.getElementById("messageform").reset();
-
-    }, 5000); // set timout
-    // this will prevent the submit
-    return false;
-};
-
-$("li").on('click', function() {
-    $("li").removeClass("active");
-    $(this).toggleClass("active");
-});
-
-function searchValidation() {
-    var fn = document.getElementById('searchfield').value;
-    if (fn == "") {
-        alert('Please Enter Team Name');
-        document.getElementById('searchfield').style.borderColor = "red";
-        return false;
-
-    } else if (/^[0-9]+$/.test(document.getElementById("searchfield").value)) {
-        alert("Team Name Contains Numbers!");
-        document.getElementById('searchfield').style.borderColor = "red";
-        return false;
-
-    } else if (fn.length <= 2) {
-        alert('Team Name is To Short');
-        document.getElementById('searchfield').style.borderColor = "red";
-        return false;
-
-    } else {
-        document.getElementById('searchfield').style.borderColor = "green";
-    }
-
-    searchsubmit();
-};
-
-
-function searchsubmit() {
-
-    const url = "http://api.football-data.org/v2/competitions/2021/standings";
-    fetch(url, {
-            method: "GET",
-            headers: {
-                "X-Auth-Token": "d38ac557ec364cf79e21a985e5d1cf8c",
-            }
-        })
-        .then(resp => {
-            return resp.json();
-        })
-        .then(function(data) {
-
-        });
-
 }
